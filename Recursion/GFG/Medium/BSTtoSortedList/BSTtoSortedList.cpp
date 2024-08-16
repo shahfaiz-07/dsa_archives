1225b3201602bb5d7724c01a168f21a0624bb04a
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-08-16 23:07:17 +0530

    added solution for flattening a BST

---
+++ Recursion/GFG/Medium/BSTtoSortedList/BSTtoSortedList.cpp
@@ -0,0 +1,44 @@
+// https://www.geeksforgeeks.org/problems/flatten-bst-to-sorted-list--111950/
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+// Main code
+Node *flattenBST(Node *root)
+{
+    if (root)
+    {
+        Node *left = flattenBST(root->left); // trust that this code gives you the head of left sub tree which is flattened
+        root->left = nullptr;
+        Node *head = root;
+        if (left)
+        {
+            head = left;
+            while (left->right != nullptr)
+            {
+                left = left->right;
+            }
+            left->right = root;
+        }
+        Node *right = flattenBST(root->right);
+        root->right = right;
+        return head;
+    }
+    return nullptr;
+}
+int main(void)
+{
+
+    return 0;
+}
---
+++ Recursion/GFG/Medium/BSTtoSortedList/BSTtoSortedList.java
@@ -0,0 +1,37 @@
+// https://www.geeksforgeeks.org/problems/flatten-bst-to-sorted-list--111950/
+
+public class BSTtoSortedList {
+    // Main Code
+    static Node flattenBST(Node root) {
+        if (root != null) {
+            Node left = flattenBST(root.left); // trust that this code gives you the head of left sub tree which is flattened
+            root.left = null;
+            Node head = root;
+            if (left != null) {
+                head = left;
+                while (left.right != null) {
+                    left = left.right;
+                }
+                left.right = root;
+            }
+            root.right = flattenBST(root.right);
+            return head;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
