6826b7acc072f35b299d84b228bd4afbeeb5c6ff
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-09-02 01:49:15 +0530

    Added island problem of both gfg and leetcode

---
+++ Graph/GFG/Medium/Islands/Islands.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(vector<vector<char>> &grid, int i, int j) {
+    if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+    dfs(grid, i - 1, j - 1);
+    dfs(grid, i + 1, j + 1);
+    dfs(grid, i + 1, j - 1);
+    dfs(grid, i - 1, j + 1);
+}
+int numIslands(vector<vector<char>> &grid)
+{
+    // Code here
+    int count = 0;
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[0].size(); j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                count++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+    return count;
+}
+int main(void)
+{
+
+    return 0;
+}
