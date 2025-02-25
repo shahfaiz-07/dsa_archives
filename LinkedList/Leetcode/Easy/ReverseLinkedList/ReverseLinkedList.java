b6673ca9a96c03d5ecc2284730a03c7664c8df92
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2025-02-25 13:59:11 +0530

    added solution for reverse linked list problem

---
+++ LinkedList/Leetcode/Easy/ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reverse-linked-list/
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null, curr = head;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
