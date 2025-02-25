86d4aa4eb1428a9a35cc08477a1e7324604fcd2c
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2025-02-26 00:26:09 +0530

    added solution for invert binary tree problem

---
+++ BinaryTree/Leetcode/Easy/InvertTree/InvertTree.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+public class InvertTree {
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+
+        return root;
+    }
+}
