2436a2c398dcc991214ebf16a2cae55e18286aa9
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2025-02-26 01:57:09 +0530

    added solution for same tree problem

---
+++ BinaryTree/Leetcode/Easy/SameTree/SameTree.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/same-tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    if (!p)
+        return !q;
+    if (!q)
+        return !p;
+    if (p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
---
+++ BinaryTree/Leetcode/Easy/SameTree/SameTree.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/same-tree
+
+public class SameTree {
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null) {
+            return q == null;
+        }
+        if (q == null) {
+            return p == null;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
