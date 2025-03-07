b974d08d30305b888c19a2d5259b783e9da10dea
S. Faizaan Hussain <hussainfaizaan07@gmail.com>
2025-03-08 03:12:40 +0530

    added solution for Allocate Books problem

---
+++ BinarySearch/CodingNinjas/Medium/AllocateBooks/AllocateBooks.cpp
@@ -0,0 +1,50 @@
+// https://www.naukri.com/code360/problems/allocate-books_1090540
+#include<bits/stdc++.h>
+using namespace std;
+bool canAllocate(vector<int> &arr, int m, int mid)
+{
+    int pages = 0, s = 1;
+    for (int book : arr)
+    {
+        if (pages + book > mid)
+        {
+            s++;
+            if (s > m)
+                return false;
+            pages = book;
+        }
+        else
+        {
+            pages += book;
+        }
+    }
+    return true;
+}
+int findPages(vector<int> &arr, int n, int m)
+{
+    // Write your code here.
+    if (n < m)
+    {
+        return -1;
+    }
+    int l = 0, r = 0;
+    for(int &ele: arr) {
+        l = max(ele, l);
+        r += ele;
+    }
+    int ans = -1;
+    while (l <= r)
+    {
+        int mid = (r + l) / 2;
+        if (canAllocate(arr, m, mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
---
+++ BinarySearch/CodingNinjas/Medium/AllocateBooks/AllocateBooks.java
@@ -0,0 +1,41 @@
+// https://www.naukri.com/code360/problems/allocate-books_1090540
+
+import java.util.ArrayList;
+
+public class AllocateBooks {
+    static boolean canAllocate(ArrayList<Integer> arr, int m, int mid) {
+        int pages = 0, s = 1;
+        for(int book: arr) {
+            if(pages + book > mid) {
+                s++;
+                if(s > m) return false;
+                pages = book;
+            } else {
+                pages += book;
+            }
+        }
+        return true;
+    }
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // Write your code here.
+        if(n < m) {
+            return -1; 
+        }
+        int l = 0, r = 0;
+        for(int ele: arr) {
+            l = Math.max(ele, l);
+            r += ele;
+        }
+        int ans = -1;
+        while(l <= r) {
+            int mid = (r + l)/2;
+            if(canAllocate(arr, m, mid)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
