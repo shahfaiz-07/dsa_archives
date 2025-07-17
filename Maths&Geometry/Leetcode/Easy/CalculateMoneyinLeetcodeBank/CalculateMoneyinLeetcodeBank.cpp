8e86df6c7a46f1b713b39fca5efae45700e6724a
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2025-07-18 00:05:11 +0530

    Add implementation and metadata for Calculate Money in Leetcode Bank problem

---
+++ Maths&Geometry/Leetcode/Easy/CalculateMoneyinLeetcodeBank/CalculateMoneyinLeetcodeBank.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+#include <bits/stdc++.h>
+using namespace std;
+
+int totalMoney(int n)
+{
+    int weeks = n / 7, rem = n % 7;
+    // ans = otherWeek(forms an AP where, a = 28, d = 7, n = weeks)
+    // + finalWeek(forms an AP where, a = weeks + 1, d = 1, n = rem)
+    return (weeks * (2 * 28 + (weeks - 1) * 7)) / 2 + (rem * (2 * weeks + rem + 1)) / 2;
+}
